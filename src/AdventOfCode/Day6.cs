2018b81ffd234944efbfba1c0a28f5084f0941dc
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2022-12-10 16:13:01 +0100

    AOC Day 6

---
+++ src/AdventOfCode/Day6.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventCalendar2022.AdventOfCode;
+internal class Day6
+{
+    public static void Run()
+    {
+        var datastream = File.ReadAllText("AdventOfCode/Data/Day6Input.txt");
+        var startOfPacketCharsProcessed = 0;
+        for (var scanIndex = 0; scanIndex < datastream.Length; scanIndex++)
+        {
+            startOfPacketCharsProcessed++;
+            if (datastream.Substring(scanIndex, 4).ToHashSet().Count == 4)
+            {
+                startOfPacketCharsProcessed += 3;
+                break;
+            }
+        }
+        Console.WriteLine($"AdventOfCode Day 6 Part 1 result: {startOfPacketCharsProcessed}");
+
+        var startOfMessageCharsProcessed = 0;
+        for (var scanIndex = 0; scanIndex < datastream.Length; scanIndex++)
+        {
+            startOfMessageCharsProcessed++;
+            if (datastream.Substring(scanIndex, 14).ToHashSet().Count == 14)
+            {
+                startOfMessageCharsProcessed += 13;
+                break;
+            }
+        }
+        Console.WriteLine($"AdventOfCode Day 6 Part 2 result: {startOfMessageCharsProcessed}");
+    }
+}
