4f80c7847ff115574878f4e67df8d427e0bbf407
Øyvind Heddeland Instefjord <oyvind.heddeland.instefjord@phonero.no>
2022-12-12 07:56:09 +0100

    AOC Day 12 prep

---
+++ src/AdventOfCode/Day12.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventCalendar2022.AdventOfCode;
+internal class Day12
+{
+    public static void Run()
+    {
+        var commandQueue = File.ReadAllLines("AdventOfCode/Data/Day12Input.txt");
+        //Console.WriteLine($"AdventOfCode Day 12 Part 1 result: {signalStrengthSum}");
+        //Console.WriteLine($"AdventOfCode Day 12 Part 2 result:");
+    }
+}
