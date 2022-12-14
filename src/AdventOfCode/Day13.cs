e234da00b645836b6ead3929a0955750ff35c4e2
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2022-12-14 16:57:51 +0100

    AOC Day 13 prep

---
+++ src/AdventOfCode/Day13.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventCalendar2022.AdventOfCode;
+internal class Day13
+{
+    public static void Run()
+    {
+        var commandQueue = File.ReadAllLines("AdventOfCode/Data/Day13Input.txt");
+        //Console.WriteLine($"AdventOfCode Day 13 Part 1 result: {signalStrengthSum}");
+        //Console.WriteLine($"AdventOfCode Day 13 Part 2 result:");
+    }
+}
